3425a3c7f3620a2767bf3133ed56117bdf2b6431
ARYAN BHARDWAJ <aryanwibhardwaj@gmail.com>
2026-05-10 22:32:32 +0530

    Add function to concatenate array with its reverse
    Implemented a function to concatenate an array with its reverse and included a main function for user input and output.

---
+++ Array/3925.-Concatenate-Array-With-Reverse.cpp
@@ -0,0 +1,53 @@
+/*
+Time Complexity: O(n)
+- First loop runs n times.
+- Second loop also runs n times.
+- Total = O(2n) → O(n)
+
+Space Complexity: O(n)
+- Extra vector 'result' stores 2n elements.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> concatWithReverse(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<int> result;
+        for (int i = 0; i < n; i++) {
+
+            result.push_back(nums[i]);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+
+            result.push_back(nums[i]);
+        }
+        return result;
+    }
+};
+
+int main() {
+
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    Solution obj;
+    vector<int> result = obj.concatWithReverse(nums);
+    cout << "Concatenated Array with Reverse: ";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
