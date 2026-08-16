c9a309965f68c958cdc0b2bb2ce66189bd9a9e87
ARYAN BHARDWAJ <aryanwibhardwaj@gmail.com>
2026-08-16 11:09:54 +0530

    Add optimal solution for finding middle of linked list
    Implement a solution to find the middle of a linked list using the slow and fast pointer technique. Includes functions to convert an array to a linked list and print the linked list.

---
+++ Leetcode-Questions/slow-fast-pointer/Question-Number-876/optimalSolution.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+  
+    Node* middleOfLinkedList(Node*  head){
+
+        if(head ==  NULL || head->next == NULL ) return head  ;
+        Node* slow  = head  ;
+        Node* fast  = head  ;
+        while ( fast != NULL && fast->next != NULL ){
+            slow = slow->next ;
+            fast = fast->next->next ;
+        }
+        return slow ;
+    }
+    
+};
+
+
+Node *convertArrayToLinkedList(vector<int> &arr)
+{
+    if (arr.empty())  return NULL;
+    Node *head = new Node(arr[0]);
+    Node *current = head;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i], nullptr);
+        current->next = temp;
+        current = temp;
+    }
+    return head;
+}
+
+void  printLinkedList(Node*  head){
+    Node* temp = head ;
+    while (temp != NULL ){
+        cout <<temp->data <<" ";
+        temp = temp->next ;
+    }
+    cout << endl ;
+}
+
+int main()
+{
+    vector<int> arr{};
+    cout << "Enter the Number of NOde of Linked List : - ";
+    int x;
+    cin >> x;
+    for (int i = 0; i < x; i++)
+    {
+        int value;
+        cin >> value;
+        arr.push_back(value);
+    }
+    Node *head = convertArrayToLinkedList(arr);
+    Node* middle = head->middleOfLinkedList(head);
+    printLinkedList(middle);
+}
